f06357c118b2530b4a96d54eb20b52718ba0bce8
paulforce18 <39962832+paulforce18@users.noreply.github.com>
2021-02-14 01:30:19 +0800

    Create owner.js

---
+++ owner.js
@@ -0,0 +1,70 @@
+[
+    {
+        "firtName": "John Paul",
+        "middleName": "Cabe",
+        "lastName": "Alimango",
+        "age": "22",
+        "birthDate": "new Date('1998-09-18')",
+        "address": "Lias Marilao, Bulacan",
+        "mobileNo": "09382610657",
+        "email": "undergroundph12@gmail.com",
+    
+        "socialMediaAccounts": [
+            {
+                "platform": "twitter",
+                "image": "./images/icons8-twitter-48.png",
+                "link": ""
+            },
+            {   
+                "platform": "facebook",
+                "image": "./images/icons8-facebook-48.png",
+                "link": "https://www.facebook.com/tortangokra18/"
+            },
+            {
+                "platform": "linkedin",
+                "image": "./images/icons8-linkedin-48.png",
+                "link": ""
+            }
+        ],
+
+        "skillsDescription": "I can create a simple static website with responsive layout",
+        "skills": [
+            {
+                "skill": "javascript",
+                "image": "./images/javascript.svg"
+            },
+            {
+                "skill": "python",
+                "image": "./images/python.svg"
+            },
+            {
+                "skill": "html5",
+                "image": "./images/html5.svg"
+            },
+            {
+                "skill": "css3",
+                "image": "./images/css3.svg"
+            }
+        ],
+
+        "worksDescription": "This works are based on my college projects & Activities",
+        "worksImages": [
+            {
+                "image": "./images/monsterheel.png",
+                "alt": "name-picker"
+            },
+            {
+                "image": "./images/money.png",
+                "alt": "money-classifier"
+            },
+            {
+                "image": "./images/jp.jpg",
+                "alt": "nothing"
+            },
+            {
+                "image": "./images/jp.jpg",
+                "alt": "nothing"
+            }
+        ]
+    }
+]
