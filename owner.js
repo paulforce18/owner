6243db20c58eb198f5709d2ba8f3f57e3d94fcbb
paulforce18 <39962832+paulforce18@users.noreply.github.com>
2021-02-15 18:13:15 +0800

    Update owner.js

--- owner.js
+++ owner.js
@@ -67,6 +67,12 @@
                 "worksDescription": "",
                 "image": "https://raw.githubusercontent.com/paulforce18/owner/main/project/jp.jpg",
                 "alt": "nothing"
+            },
+            {
+                "name": "",
+                "worksDescription": "",
+                "image": "https://raw.githubusercontent.com/paulforce18/owner/main/project/jp.jpg",
+                "alt": "nothing"
             }
         ]
     }
